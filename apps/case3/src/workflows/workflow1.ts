ea881d8461ab37f41e8b745b5980e9538160e442
Gerry Tang <gerry@bookipi.com>
2025-06-11 01:49:28 +1000

    add case3

---
+++ apps/case3/src/workflows/workflow1.ts
@@ -0,0 +1,21 @@
+import { proxyActivities, sleep } from "@temporalio/workflow";
+import type * as activities from "../activities";
+const { sum } = proxyActivities<{
+  [funcName in keyof typeof activities]: (typeof activities)[funcName];
+}>({
+  startToCloseTimeout: "1m",
+  retry: {
+    backoffCoefficient: 2,
+    initialInterval: 60000, // 1 minute
+    maximumAttempts: 2,
+  },
+});
+
+export const sumWorkflow = async (numbers: number[]): Promise<number> => {
+  let base = 0;
+  // console.log("base before:", base);
+  await sleep(30000);
+  // console.log("base:", base);
+  const total = await sum(numbers);
+  return base + total;
+};
